4005ea3591d3079d91ddb4b5b9cb792258830fa9
MD KHORSHED ALAM <130845087+MKA-001@users.noreply.github.com>
2024-11-17 16:37:34 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+print('Hello world')
+2+2
+print('2+2=4')
+print(3*4)
+print(type(23.34))
+print(type(23))
+x = 34
+y = 23
+sum = x + y
+print("Sum is:", sum) 
+product = x*y
+print("Product is:",)
+Div = x/y
+print("Division is:", Div) 
+Quo = x//y
+print('Quotient is:',Quo) 
+Remainder_mod = x%y
+print('Remainder/modulas is:', Remainder_mod)
+Exp = x**y
+print('Exponent is:', Exp)
+z = 45
+if x<y and y<z:
+    print('Largest number is:',z)
+print(x==y)
+print(x!=y)
+print(y>=z)
+print(z<=y)
+print(x<y and y<z)
+print((x<y) or (y<z))
+text = "Kamal is a good boy, he goes to school regurarly"
+print('goes' in text)
+print('field' in text)
+num_1 = int(input("Enter the 1st number:"))
+num_2 = int(input("Enter the 2nd number:"))
+sum = num_1+num_2
+print("Sum is :", sum)
+diff = num_1 - num_2
+print("Difference is :", diff)
+product = num_1*num_2
+print("The product is :", product)
+age = int(input("Enter your age:"))
+age_after_5_years = age + 5
+print('Obtained age is:', age_after_5_years)
+print("20",str(24)) 
+print(type(diff))
+print(type(False))
+fruits = ['apple', 'bananan', 'mango']
+print('mango' in fruits)
+std_num = 30
+if (std_num >= 25):
+    print('Great presense') 
+elif std_num >=20:
+    print("Good presense")
+elif std_num >= 15:
+    print("Not a bad presense")
+else:
+    print("worse presense")
